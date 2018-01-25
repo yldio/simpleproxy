44345dccd5b2ef7e2829b493f554ca0b274659f4
Nuno Job <nuno@yld.io>
2018-01-25 18:09:17 +0000

    code that might actually work

--- serviceworker.js
+++ serviceworker.js
@@ -1,2 +1,15 @@
-ServiceWorkerRegistration.unregister().then(window.reload);
-
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.keys().map(function(cacheName) {
+        return caches.delete(cacheName);
+      })
+    );
+  });
+  
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().map(function(cacheName) {
+        return caches.delete(cacheName);
+      })
+    );
+  });
